fadd49dba629d655af23d89193c33854ad0340d5
dennisgk <dennisgk2005@gmail.com>
2025-12-15 09:16:20 -0500

    Connect immediately when scan already stopped

--- main/ble-test-g1.cpp
+++ main/ble-test-g1.cpp
@@ -819,7 +819,12 @@ static void gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param) 
             }
 
             // Stop scan then connect (python-style)
-            stop_scan();
+            if (g_scan_active) {
+                stop_scan();
+            } else {
+                ESP_LOGI(TAG, "Scan already stopped; connecting now");
+                connect_target();
+            }
         }
         break;
     }
